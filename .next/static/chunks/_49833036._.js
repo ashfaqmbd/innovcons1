1bfc11029564f15a7982e7f652c9f673b01c4024
ashfaqmbd <ashfaqmbd@gmail.com>
2025-06-18 21:00:49 +0500

    navbar updated

--- .next/static/chunks/_49833036._.js
+++ .next/static/chunks/_49833036._.js
@@ -78,7 +78,7 @@ function Navbar() {
         className: "bg-white sticky top-0 z-10 shadow-lg",
         children: [
             /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
-                className: "h-8 md:h-20 flex justify-between items-center ",
+                className: "h-4 md:h-20 flex justify-between items-center ",
                 children: [
                     /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
                         className: "text-white font-bold text-3xl mx-4 leading-3 tracking-tight ",
